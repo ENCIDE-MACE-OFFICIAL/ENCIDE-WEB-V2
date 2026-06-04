afec6087dff74485e45448f6e7677537f199a9ad
Reeba Roy <reebaroy07@gmail.com>
2026-06-04 12:54:23 +0300

    Removed filler names for icl winners and added Geoguessr event winners

--- src/Pages/icl-dashboard/scoreData.js
+++ src/Pages/icl-dashboard/scoreData.js
@@ -7,11 +7,11 @@ export const eventsData = [
   {
     id: "ai-or-not-ai",
     name: "Ai or Not Ai?",
-    scores: { CS: 100, AIML: 34, "DS(A)": 53, "DS(B)": 80,"EEE":46, "ECE":71 ,CIVIL: 20, MECH: 40 },
+    scores: { CS: 0, AIML: 0, "DS(A)": 0, "DS(B)": 0,"EEE":0, "ECE":0 ,CIVIL: 0, MECH: 0 },
     winners: [
-      { name: "Aromal Sanil", department: "CS" },
-      { name: "Sai Madhav", department: "DS(B)" },
-      { name: "Affan Ahamed", department: "DS(A)" }
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A)" }
     ]
   },
   {
@@ -19,9 +19,9 @@ export const eventsData = [
     name: "GEOGUESSR: CAMPUS & TECH EDITION",
     scores: { CS: 0, AIML: 0, "DS(A)": 0, "DS(B)": 0, "EEE":0, "ECE":0 ,CIVIL: 0, MECH: 0 },
     winners: [
-      { name: "Ethan Hunt", department: "CS" },
-      { name: "Mia Wallace", department: "AIML" },
-      { name: "Oliver Queen", department: "DS(A)" }
+      { name: "Aromal Sanil", department: "CS" },
+      { name: "Sai Madhav", department: "DS(B)" },
+      { name: "Affan Ahamed", department: "DS(A)" }
     ]
   },
   {
@@ -29,9 +29,9 @@ export const eventsData = [
     name: "Photo and Caption Contest",
     scores: { CS: 0, AIML: 0, "DS(A)": 0, "DS(B)": 0, "EEE":0, "ECE":0 ,CIVIL: 0, MECH: 0 },
     winners: [
-      { name: "Lucas Scott", department: "CS" },
-      { name: "Emma Swan", department: "AIML" },
-      { name: "Aria Montgomery", department: "DS(A)" }
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" }
     ]
   },
   {
@@ -39,9 +39,9 @@ export const eventsData = [
     name: "Prompt Golf",
     scores: { CS: 0, AIML: 0, "DS(A)": 0, "DS(B)": 0, "EEE":0, "ECE":0 ,CIVIL: 0, MECH: 0 },
     winners: [
-      { name: "Leo Valdez", department: "CS" },
-      { name: "Piper McLean", department: "AIML" },
-      { name: "Jason Grace", department: "DS(A)" }
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" }
     ]
   },
   {
@@ -49,9 +49,9 @@ export const eventsData = [
     name: "WIKIPEDIA SPEEDRUN (The Tech Rabbit Hole)",
     scores: { CS: 0, AIML: 0, "DS(A)": 0, "DS(B)": 0, "EEE":0, "ECE":0 ,CIVIL: 0, MECH: 0 },
     winners: [
-      { name: "Percy Jackson", department: "CS" },
-      { name: "Annabeth Chase", department: "AIML" },
-      { name: "Grover Underwood", department: "DS(A)" }
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" }
     ]
   },
   {
@@ -59,9 +59,9 @@ export const eventsData = [
     name: "Typing Competition",
     scores: { CS: 0, AIML: 0, "DS(A)": 0, "DS(B)": 0, "EEE":0, "ECE":0 ,CIVIL: 0, MECH: 0 },
     winners: [
-      { name: "Barry Allen", department: "CS" },
-      { name: "Iris West", department: "AIML" },
-      { name: "Cisco Ramon", department: "DS(A)" }
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" }
     ]
   },
   {
@@ -69,9 +69,9 @@ export const eventsData = [
     name: "AI Story Generator",
     scores: { CS: 0, AIML: 0, "DS(A)": 0, "DS(B)": 0, "EEE":0, "ECE":0 ,CIVIL: 0, MECH: 0 },
     winners: [
-      { name: "Tony Stark", department: "CS" },
-      { name: "Bruce Banner", department: "AIML" },
-      { name: "Peter Parker", department: "DS(A)" }
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" }
     ]
   },
   {
@@ -79,9 +79,9 @@ export const eventsData = [
     name: "Poster Designing",
     scores: { CS: 0, AIML: 0, "DS(A)": 0, "DS(B)": 0, "EEE":0, "ECE":0 ,CIVIL: 0, MECH: 0 },
     winners: [
-      { name: "Steve Rogers", department: "CS" },
-      { name: "Natasha Romanoff", department: "AIML" },
-      { name: "Clint Barton", department: "DS(A)" }
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" }
     ]
   },
   {
@@ -89,9 +89,9 @@ export const eventsData = [
     name: "Reel Making",
     scores: { CS: 0, AIML: 0, "DS(A)": 0, "DS(B)": 0, "EEE":0, "ECE":0 ,CIVIL: 0, MECH: 0 },
     winners: [
-      { name: "Clark Kent", department: "CS" },
-      { name: "Lois Lane", department: "AIML" },
-      { name: "Jimmy Olsen", department: "DS(A)" }
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" }
     ]
   },
   {
@@ -99,9 +99,9 @@ export const eventsData = [
     name: "Debugging",
     scores: { CS: 0, AIML: 0, "DS(A)": 0, "DS(B)": 0, "EEE":0, "ECE":0 ,CIVIL: 0, MECH: 0 },
     winners: [
-      { name: "Bruce Wayne", department: "CS" },
-      { name: "Dick Grayson", department: "AIML" },
-      { name: "Barbara Gordon", department: "DS(A)" }
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" }
     ]
   },
   {
@@ -109,9 +109,9 @@ export const eventsData = [
     name: "Digital Marketing Challenge",
     scores: { CS: 0, AIML: 0, "DS(A)": 0, "DS(B)": 0, "EEE":0, "ECE":0 ,CIVIL: 0, MECH: 0 },
     winners: [
-      { name: "Diana Prince", department: "CS" },
-      { name: "Arthur Curry", department: "AIML" },
-      { name: "Victor Stone", department: "DS(A)" }
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" },
+      { name: "N/A", department: "N/A" }
     ]
   }
 ];
